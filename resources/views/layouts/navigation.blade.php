1d7e05af980d9d4138bf371ebc69dfba4d9436ce
SaddamHosen42 <saddamhosen1433@gmail.com>
2026-03-14 05:58:04 +0600

    update:app logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-8 w-auto fill-current text-gray-600" />
+                        <img src="{{ asset('img/hstu-logo.png') }}" alt="HSTU logo" class="block h-12 w-auto">
                     </a>
                 </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         <div class="w-full flex items-center justify-between">
             <a class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
                href="{{ route('welcome') }}">
-                <x-application-logo class="block h-8 pr-2 w-auto fill-current" />
+                <x-application-logo class="block h-14 pr-2 w-auto fill-current" />
                 <span class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">HSTU-OBE</span>
             </a>
 
